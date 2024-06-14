8075aaaf8284032cb5dbb441ecc54fb3151495bd
VoxelPrismatic <danielpetrov@outlook.com>
2024-06-13 23:16:36 -0500

    init (non functional)

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+DOCUMENT_ROOT=/var/www/sources
+
+# Take site offline
+echo "Taking site offline..."
+touch $DOCUMENT_ROOT/maintenance.file
+
+# Swap over the content
+echo "Deploying content..."
+mkdir -p $DOCUMENT_ROOT/Floatplane
+cp floatplane.png $DOCUMENT_ROOT/Floatplane
+cp FloatplaneConfig.json $DOCUMENT_ROOT/Floatplane
+cp FloatplaneScript.js $DOCUMENT_ROOT/Floatplane
+sh sign.sh $DOCUMENT_ROOT/Floatplane/FloatplaneScript.js $DOCUMENT_ROOT/Floatplane/FloatplaneConfig.json
+
+# Notify Cloudflare to wipe the CDN cache
+echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{"files":["https://plugins.grayjay.app/Floatplane/floatplane.png", "https://plugins.grayjay.app/Floatplane/FloatplaneConfig.json", "https://plugins.grayjay.app/Floatplane/FloatplaneScript.js"]}'
+
+# Take site back online
+echo "Bringing site back online..."
+rm $DOCUMENT_ROOT/maintenance.file
